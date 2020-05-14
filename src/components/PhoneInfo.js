bf48a99990c75acd10927dc9e5ad343f29a54cf3
BadCandy <christ99791@gmail.com>
2020-05-14 20:02:49 +0900

    [feature] Add to function removing phone inforation

--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class PhoneInfo extends Component {
+
+    handleRemove = () => {
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+    
     render() {
         const { name, phone, id } = this.props.info;
 
@@ -13,7 +19,8 @@ class PhoneInfo extends Component {
         return (
             <div style={style}>
                 <div><b>{name}</b></div>
-                <div>{phone}</div>        
+                <div>{phone}</div>  
+                <button onClick={this.handleRemove}>삭제</button>      
             </div>
         );
     }
